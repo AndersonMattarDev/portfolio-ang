9be73b9968e87846804735e18367b9de15e4c109
Anderson <andmat7745@gmail.com>
2024-12-20 10:50:28 -0300

    inclisão das paginas e componentes nav e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { SkillsComponent } from './pages/skills/skills.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
-import { ExemploComponenteComponent } from './_components/exemplo-componente/exemplo-componente.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { FooterComponent } from './_components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent,
-    SkillsComponent,
-    ProjectsComponent,
-    ExemploComponenteComponent
+    NavbarComponent,
+    FooterComponent,
+
   ],
   imports: [
     BrowserModule,
